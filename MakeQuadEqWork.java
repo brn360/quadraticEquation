a2faa9a75c9db03dca69e5165bb81c4922700cfd
Trent Bitterman <Trent251@users.noreply.github.com>
2015-10-20 19:13:44 -0600

    Update MakeQuadEqWork.java

--- MakeQuadEqWork.java
+++ MakeQuadEqWork.java
@@ -23,6 +23,9 @@ public class MakeQuadEqWork implements ActionListener, Runnable {
         if(command.equals("Random Numbers")) {
             addRandomNumbers();
         }
+        if(command.equals("Your Numbers")) {
+            yourNumbers();
+        }
     }
     
     public void addRandomNumbers() {
@@ -47,15 +50,17 @@ public class MakeQuadEqWork implements ActionListener, Runnable {
         gui.manual.setEnabled(true);
         gui.random.setEnabled(true);
         
-        float a = Integer.parseInt("0" + gui.inputboxes[0].getText());
-        float b = Integer.parseInt("0" + gui.inputboxes[1].getText());
-        float c = Integer.parseInt("0" + gui.inputboxes[2].getText());
+        double a = Integer.parseInt("0" + gui.inputboxes[0].getText());
+        double b = Integer.parseInt("0" + gui.inputboxes[1].getText());
+        double c = Integer.parseInt("0" + gui.inputboxes[2].getText());
         
-        float answer1 = -b + Math.sqrt((b * b)-4 * a * c)/(2 * a);
-        float answer2 = -b - Math.sqrt((b * b)-4 * a * c)/(2 * a);
+        float answer1 = (float) (-b + Math.sqrt((b * b)-4 * a * c)/(2 * a));
+        float answer2 = (float) (-b - Math.sqrt((b * b)-4 * a * c)/(2 * a));
         
         gui.answerone.setText("" + answer1);
         gui.answertwo.setText("" + answer2);
+        
+        checkAnswer();
     }
     
     void clearAllFields() {
@@ -74,14 +79,24 @@ public class MakeQuadEqWork implements ActionListener, Runnable {
         
     }
     
-    boolean numberGone(int num, JTextField[] pastNums, int count) {
-        for(int i = 0; i < count; i++) {
-            if(Integer.parseInt(pastNums[i].getText()) == num) {
-                return true;
-            }
+    public void checkAnswer() {
+        String answer = gui.answerone.getText();
+        String noNaN = answer.replace("NaN", "Does Not Exist");
+        gui.answerone.setText(noNaN);
+        
+        String answer1 = gui.answertwo.getText();
+        String noNaN1 = answer1.replace("NaN", "Does Not Exist");
+        gui.answertwo.setText(noNaN1);
+    }
+    
+    void yourNumbers() {
+        for(int i = 0; i < 3; i++) {
+            gui.inputboxes[i].setText(null);
         }
-        return false;
-        }
+        gui.answerone.setText(null);
+        gui.answertwo.setText(null);
+        gui.solvebutton.setEnabled(true);
+    }
     
     @Override
     public void run() {
